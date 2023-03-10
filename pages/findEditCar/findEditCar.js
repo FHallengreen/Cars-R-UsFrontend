53369292165d2d37697b1222ecbdbb5e8693eb4e
FHallengreen <fred70p9@stud.kea.dk>
2023-03-10 11:40:23 +0100

    all cars working

--- pages/findEditCar/findEditCar.js
+++ pages/findEditCar/findEditCar.js
@@ -1,9 +1,11 @@
 import { API_URL} from "../../settings.js"
-
 //Add id to this URL to get a single user
 const URL = `${API_URL}/cars`
+import { sanitizeStringWithTableRows } from "../../utils.js"
 
-export function initFindEditCar(){
+
+
+export async function initFindEditCar(){
 
 
   
