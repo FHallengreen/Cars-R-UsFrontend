64da3bf6a3490040726d7350ea47e8053c4de958
FHallengreen <fred70p9@stud.kea.dk>
2023-03-11 14:24:22 +0100

    changes

--- pages/cars/cars.js
+++ pages/cars/cars.js
@@ -3,6 +3,8 @@ const URL = API_URL + "/cars";
 import { sanitizeStringWithTableRows } from "../../utils.js";
 
 export async function initCars() {
+  document.getElementById("loading").classList.remove("d-none");
+  try{
   const cars = await fetch(URL).then((res) => res.json());
   const tableRowsStr = cars
     .map(
@@ -19,4 +21,12 @@ export async function initCars() {
 
   const okRows = sanitizeStringWithTableRows(tableRowsStr);
   document.getElementById("table-rows").innerHTML = okRows;
+    }
+    catch(error){
+      alert(error);
+    }
+    finally {
+      // hide the spinner
+      document.getElementById("loading").classList.add("d-none");
+    }
 }
